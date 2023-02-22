b38f8b727ff50841abe334fc61f2ed255a2a68e0
ahmed-oumerhouch <101425292+ahmed-oumerhouch@users.noreply.github.com>
2023-02-21 22:58:50 -0400

    update

--- Main.java
+++ Main.java
@@ -16,13 +16,13 @@ public class Main {
 
 
         // text Allo dans le png
-        String nomFichierEntre = "test.txt";
+        String nomFichierEntre = "testahmed.txt";
         TableFrequence tableFrequence = new TableFrequence(nomFichierEntre);
         int tab[] = tableFrequence.getTableFrequence();
         HuffmanTree tree = new HuffmanTree(tab);
         HuffmanTree.Node root = tree.getRoot();
 //         SI TU VEUX VOIR L'ARBRE RESSEMBLE À QUOI
-        afficherArbre(root, "", true);
+//        afficherArbre(root, "", true);
 
         Map<Integer, String> huffmanCodes = tree.getHuffmanCodes(root);
         System.out.println("les chemin d'accés de l'arbre");
@@ -40,62 +40,11 @@ public class Main {
         }
         System.out.println("");
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-        // Sérialiser l'objet en binaire
+        // Compresser
         Compress compress = new Compress(tree, huffmanCodes, tableFrequence.getBytes());
         compress.compressfile("myclass.bin");
-
-        System.out.println("L'objet a été sérialisé en binaire:");
-        BitInputStream in = new BitInputStream("myclass.bin");
-        while (true) {
-            int bit = in.readBit();
-            if (bit == -1)
-                break;
-            System.out.print(bit);
-        }
-        in.close();
-
-        // Désérialiser l'objet depuis le fichier binaire et stocker dans une variable
-
-        HuffmanTree obj2 = null;
-        try {
-            FileInputStream fileIn = new FileInputStream("myclass.bin");
-            DataInputStream dataInputStream = new DataInputStream(fileIn);
-            obj2 = (HuffmanTree) new ObjectInputStream(dataInputStream).readObject();
-            dataInputStream.close();
-            fileIn.close();
-            System.out.println("L'objet a été désérialisé depuis myclass.bin : " + obj2);
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        HuffmanTree.Node root2 = obj2.getRoot();
-        Map<Integer, String> huffmanObj2 = obj2.getHuffmanCodes(root2);
-        System.out.println("les chemin d'accés de l'arbre Déserialisé");
-        for (Map.Entry<Integer, String> entry : huffmanObj2.entrySet()) {
-            System.out.println("Octet: " + entry.getKey() + ", Code de Huffman: " + entry.getValue());
-        }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-//        System.out.println("LE MAIN -----------");
-//        String fileName6 = "test.txt";
-//        int numOfReadChar_binMode = 0;
-//        File file = new File(fileName6);
-//        try (FileInputStream fileInputStream6 = new FileInputStream(file)) {
-//            int singleCharInt;
-//            char singleChar;
-//            while ((singleCharInt = fileInputStream6.read()) != -1) {
-//                singleChar = (char) singleCharInt;
-//                System.out.println(String.format("0x%X %c", singleCharInt, singleChar));
-//                numOfReadChar_binMode++;
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        System.out.println("LE MAIN -----------Nombre d'octets lus en mode binaire : " + numOfReadChar_binMode);
+        compress.decompresser("myclass.bin");
     }
 
     private static void afficherArbre(HuffmanTree.Node root, String s, boolean b) {
--- compress/Compress.java
+++ compress/Compress.java
@@ -6,6 +6,7 @@ import module.HuffmanTree;
 
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class Compress {
@@ -24,9 +25,25 @@ public class Compress {
             FileOutputStream fileOut = new FileOutputStream(outputFilePath);
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             out.writeObject(tree);
+            out.close();
+            fileOut.close();
 
-            
-            //BitOutputStream bit = new BitOutputStream(outputFilePath);
+            BitInputStream in = new BitInputStream(outputFilePath);
+            ArrayList<Integer> treebits = new ArrayList<Integer>();
+            while (true) {
+                int bit = in.readBit();
+
+                if (bit == -1) {
+                    break;
+                } else {
+                    treebits.add(bit);
+                }
+            }
+            in.close();
+            BitOutputStream bit = new BitOutputStream(outputFilePath);
+            for (int i = 0; i < treebits.size(); i++) {
+                bit.writeBit(treebits.get(i));
+            }
             String binNumber="";
             for (byte b : this.octetDuFichier) {
                 if(b<0)
@@ -36,7 +53,7 @@ public class Compress {
                     for (int i = 0; i < binNumber.length(); i++) {
                         char c = binNumber.charAt(i);
                         int value = Character.getNumericValue(c);
-                        //bit.writeBit(value);
+                        bit.writeBit(value);
                     }
                 }
                 else
@@ -46,14 +63,11 @@ public class Compress {
                     for (int i = 0; i < binNumber.length(); i++) {
                         char c = binNumber.charAt(i);
                         int value = Character.getNumericValue(c);
-                        //bit.writeBit(value);
+                        bit.writeBit(value);
                     }
                 }
             }
-            //bit.close();
-
-            out.close();
-            fileOut.close();
+            bit.close();
             System.out.println("");
             System.out.println("L'objet a été sérialisé en binaire dans myclass.bin");
         } catch (IOException e) {
@@ -62,5 +76,34 @@ public class Compress {
     }
 
 
+    public HuffmanTree decompresser(String s) {
 
+        // Désérialiser l'objet depuis le fichier binaire et stocker dans une variable
+        HuffmanTree hoffman_deserialize = null;
+        try {
+            // Ouverture du fichier pour lire les données
+            FileInputStream fileIn = new FileInputStream("myclass.bin");
+            // Création d'un objet ObjectInputStream
+            ObjectInputStream inputStream = new ObjectInputStream(fileIn);
+            // Désérialisation de l'objet HuffmanTree et de la chaîne de caractères
+            hoffman_deserialize = (HuffmanTree) inputStream.readObject();
+
+            // Fermeture des flux
+            inputStream.close();
+            fileIn.close();
+        } catch(IOException i) {
+            i.printStackTrace();
+        } catch(ClassNotFoundException c) {
+            System.out.println("Classe non trouvée");
+            c.printStackTrace();
+        }
+        HuffmanTree.Node root2 = hoffman_deserialize.getRoot();
+        Map<Integer, String> huffmanObj2 = hoffman_deserialize.getHuffmanCodes(root2);
+        System.out.println("les chemin d'accés de l'arbre Déserialisé");
+        for (Map.Entry<Integer, String> entry : huffmanObj2.entrySet()) {
+            System.out.println("Octet: " + entry.getKey() + ", Code de Huffman: " + entry.getValue());
+        }
+
+        return hoffman_deserialize ;
+    }
 }
